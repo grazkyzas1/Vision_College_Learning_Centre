a3c369a7a6b2e37c4f9467da31896aa1a85b2804
grazkyzas1 <nguyenhuusang24680@gmail.com>
2026-08-31 15:49:38 +1200

    Your update message here

--- admin/course_add.php
+++ admin/course_add.php
@@ -2,3 +2,18 @@
 // Admin Course Add Page
 require_once __DIR__ . '/../inc/admin_header.php';
 require_once __DIR__ . '/../inc/db.php';
+
+try {
+    $stmt = $pdo->query("SELECT target_audience FROM course");
+    $target_audience = $stmt->fetchAll(PDO::FETCH_ASSOC);
+} catch (PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
+try {
+    $stmt = $pdo->query("SELECT name FROM campus");
+    $campus = $stmt->fetchAll(PDO::FETCH_ASSOC);
+} catch (PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
+$success_msg = "";
+$error_msg = "";
